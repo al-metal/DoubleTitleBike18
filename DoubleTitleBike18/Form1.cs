ddf31b8e9038b1129cb0a4b0553e4601d328de8d
al-metal <al-metal@bk.ru>
2016-09-28 16:02:22 +0400

    load xlsx

--- DoubleTitleBike18/Form1.cs
+++ DoubleTitleBike18/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +30,12 @@ namespace DoubleTitleBike18
             Properties.Settings.Default.login = tbLogin.Text;
             Properties.Settings.Default.password = tbPass.Text;
             Properties.Settings.Default.Save();
+
+            FileInfo file = new FileInfo("bike18.ru.xlsx");
+            ExcelPackage p = new ExcelPackage(file);
+            ExcelWorksheet w = p.Workbook.Worksheets[1];
+            int q = w.Dimension.Rows;
+
         }
     }
 }
